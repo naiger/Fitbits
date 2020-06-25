01d10694c6ce5d70b1a82a67a64d2f6999da0ecb
Dwight <baomp2015@gmail.com>
2020-06-25 15:20:09 +1000

    added testInput file, achieved read data from txt file

--- src/Fitbits.java
+++ src/Fitbits.java
@@ -1,43 +1,51 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Fitbits {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Path path = Paths.get(".");
+		File file = new File(path.toAbsolutePath() + "/testInput.txt");
+		String inputLine = "";
+		String[] pitchSetUp = new String[2];
+		Pitch p = null;
 		
-		//initial pitch
-		System.out.println("Enter the upper-right coordinates of the pitch: ");
-		int width = scan.nextInt();
-		int length = scan.nextInt();
-		//System.out.println(width + " " + length);
-
-		Pitch p = new Pitch(width, length);
+		String[] traineeSetUp = new String[3];
+		String movement = "";
+		ArrayList<Trainee> trainees = new ArrayList<Trainee>();
+		ArrayList<String> movements = new ArrayList<String>();
 		
-		//enter multiple trainees in this pitch
-		do {
-			//initial trainee
-			System.out.println("Enter the position and orientation of trainee: ");
-			int position_x = scan.nextInt();
-			int position_y = scan.nextInt();
-			String orien = scan.next();
+		try {
+			Scanner scan = new Scanner(file);
+			if(scan.hasNextLine()) {
+				inputLine = scan.nextLine();
+				pitchSetUp = inputLine.split(" ");
+				p = new Pitch(Integer.parseInt(pitchSetUp[0]), Integer.parseInt(pitchSetUp[1]));
+			}
+			while(scan.hasNextLine()) {
+				inputLine = scan.nextLine();
+				traineeSetUp = inputLine.split(" ");
+				Trainee t = new Trainee(Integer.parseInt(traineeSetUp[0]), Integer.parseInt(traineeSetUp[1]), traineeSetUp[2]);
+				movement = scan.nextLine();				
+				trainees.add(t);
+				movements.add(movement);
+			}
+			scan.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
 
-			Trainee t = new Trainee(position_x, position_y, orien);
-			
-			//initial movements of trainee
-			System.out.println("Enter the movements of the trainee: ");
-			String m = scan.next();
-			
-			//implement movements of this trainee
-			t.move(m, p);
-			int[]position = t.getPosition();
-			
-			//print position and orientation after move
-			System.out.println("The trainee's position and orientation after move: ");
-			System.out.println(position[0]+ " " + position[1] + " " + t.getCurrentOrien() + "\n");
-			System.out.println("Enter 'exit' to exit or enter any value to continue");
-		} while(!scan.next().equals("exit"));
-
-		scan.close();
+		for (int i = 0; i < trainees.size(); i++) {
+			trainees.get(i).move(movements.get(i), p);
+			int[] position = trainees.get(i).getPosition();
+			System.out.println(position[0] + " " + position[1] + " " + trainees.get(i).getCurrentOrien());
+		}
+		
+		
 	}
 }
